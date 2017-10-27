389d66e548a56c9d01712f4916ab1cfd7ab5b32f
wlsystems <cariebadass@yahoo.com>
2017-10-26 20:50:41 -0600

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,25 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include <iostream>
 #include <QMainWindow>
+#include <QWheelEvent>
+#include <QTimeLine>
+#include <QGraphicsPixmapItem>
+#include <QUrl>
+#include <QAction>
+#include <QSignalMapper>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+#include <QPoint>
+#include <QWidget>
+#include <QPushButton>
+#include <QDebug>
+#include <QGridLayout>
+#include <QGraphicsProxyWidget>
+#include <sstream>
+#include <QVector>
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -10,12 +28,28 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+public slots:
+    void animFinished();
+    void scalingTime(qreal x);
+    void buttonClicked(const QString &text);
+
+signals:
+    void scale(qreal, qreal);
+    void pressed(const QString &text);
+
+
+protected:
 
 public:
+
+    int _numScheduledScalings;
+    void wheelEvent(QWheelEvent *event);
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
 private:
+    QPushButton *button[128][128];
+    QSignalMapper *signalMapper;
     Ui::MainWindow *ui;
 };
 
