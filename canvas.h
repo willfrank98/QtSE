76e88dd6e086f924fe6d2d4e250de2e2e070f91f
wlsystems <cariebadass@yahoo.com>
2017-10-28 06:14:01 -0600

    Update canvas.h
    added static variables because the mainwindow needs to get the colors into the canvas.

--- canvas.h
+++ canvas.h
@@ -1,31 +1,32 @@
 #ifndef CANVAS_H
 #define CANVAS_H
-
 #include <QGraphicsScene>
 #include <QGraphicsSceneMouseEvent>
-
+#include <QBrush>
 class Canvas : public QGraphicsScene
 {
     Q_OBJECT
 
 private:
+    QPen *pen;
     int size = 32;
     qreal pixSize = 480/32;
-    QBrush *brush;
     QImage *data;
     bool buttonHeld = false;
-
-    void putPixel(QPointF);
+    void putPixel(QPointF, QColor);
 
 public:
+    static QColor c1;
+    static QColor c2;
+    static QColor c1Last;
+    static QColor c2Last;
+    static QBrush *brush;
     explicit Canvas(QObject *parent = nullptr);
     explicit Canvas(int, qreal, QObject *parent = nullptr);
-
     void mouseDoubleClickEvent(QGraphicsSceneMouseEvent * mouseEvent);
     void mouseMoveEvent(QGraphicsSceneMouseEvent * mouseEvent);
     void mousePressEvent(QGraphicsSceneMouseEvent * mouseEvent);
     void mouseReleaseEvent(QGraphicsSceneMouseEvent * mouseEvent);
-
     void redraw();
     void drawGrid();
 };
