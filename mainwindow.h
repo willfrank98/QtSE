f94a84c018fe2489cd41b839416d2612e9d50c3f
wlsystems <cariebadass@yahoo.com>
2017-10-28 06:15:48 -0600

    Update mainwindow.h
    added pen and eraser icon

--- mainwindow.h
+++ mainwindow.h
@@ -4,22 +4,34 @@
 #include <QMainWindow>
 #include <QGraphicsScene>
 #include "model.h"
-
+#include <QColorDialog>
+#include <QPalette>
 namespace Ui {
 class MainWindow;
 }
 
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private slots:
+    void colorBox1Clicked();
+    void colorBox2Clicked();
+    void on_penTool_clicked();
+    void on_eraseButton_clicked();
 
 public:
-	explicit MainWindow(Model& model, QWidget *parent = 0);
+    explicit MainWindow(Model& model, QWidget *parent = 0);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
-
+    int canvasSize;
+    qreal pixelSize;
+    QColorDialog *cd ;
+    void addBackground(QGraphicsScene&);
+    void addSurface(QGraphicsScene&);
+    void addGrid(QGraphicsScene&);
     void createCanvas(int);
 };
 
