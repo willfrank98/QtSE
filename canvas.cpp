c3a3a596435fd221c087b54c97f7b2873d405bf2
wlsystems <cariebadass@yahoo.com>
2017-10-28 06:12:48 -0600

    Update canvas.cpp
    colors selector,erase, and pen

--- canvas.cpp
+++ canvas.cpp
@@ -1,6 +1,13 @@
 #include "canvas.h"
 #include <QDebug>
-
+#include <iostream>
+#include "model.h"
+using namespace std;
+QBrush *Canvas::brush;
+QColor Canvas::c1;
+QColor Canvas::c2;
+QColor Canvas::c1Last;
+QColor Canvas::c2Last;
 Canvas::Canvas(QObject *parent) : QGraphicsScene(parent)
 {
 
@@ -8,14 +15,13 @@ Canvas::Canvas(QObject *parent) : QGraphicsScene(parent)
 
 Canvas::Canvas(int size, qreal pixSize, QObject *parent) : QGraphicsScene(parent)
 {
+    Canvas::brush = new QBrush(Canvas::c1);
+    pen = new QPen(QColor::fromRgbF(0.7, 0.8, 0.9, 1.0));
     this->size = size;
     this->pixSize = pixSize;
     this->data = new QImage(size, size, QImage::Format_RGB32);
-    this->brush = new QBrush(QColor::fromRgbF(0.0, 0.0, 0.0, 1.0));
-
     // drawBg
     drawGrid();
-
 }
 
 void Canvas::redraw() {
@@ -23,9 +29,7 @@ void Canvas::redraw() {
 }
 
 void Canvas::drawGrid() {
-    QPen *pen = new QPen(QColor::fromRgbF(0.7, 0.8, 0.9, 1.0));
     pen->setWidthF(0.25);
-
     for (qreal y = 0; y < size; y++) {
         for (qreal x = 0; x < size; x++) {
             addRect(pixSize * x, pixSize * y, pixSize, pixSize, *pen);
@@ -33,17 +37,14 @@ void Canvas::drawGrid() {
     }
 }
 
-void Canvas::putPixel(QPointF point) {
+void Canvas::putPixel(QPointF point, QColor color) {
     qreal x = floor(point.x() / pixSize);
     qreal y = floor(point.y() / pixSize);
-    QPen *pen = new QPen(QColor::fromRgb(255 - brush->color().red(),
-                         255 - brush->color().green(),
-                         255 - brush->color().blue()));
-
+    Canvas::brush = new QBrush(color, Qt::SolidPattern);
     // don't place pixels outside the grid
     if (x < 0 || x >= size || y < 0 || y >= size) return;
 
-    data->setPixel(x, y, brush->color().rgb());
+    data->setPixel(x, y, Canvas::brush->color().rgb());
     addRect(pixSize * x, pixSize * y, pixSize, pixSize, *pen, *brush);
 }
 
@@ -54,15 +55,22 @@ void Canvas::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *mouseEvent)
 
 void Canvas::mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent)
 {
-    if (buttonHeld)
-        putPixel(mouseEvent->scenePos());
+    if (buttonHeld){
+        if (mouseEvent->buttons() & Qt::LeftButton)
+            putPixel(mouseEvent->scenePos(), Canvas::c1);
+        else if(mouseEvent->buttons() & Qt::RightButton)
+            putPixel(mouseEvent->scenePos(), Canvas::c2);
+    }
 //    qDebug() << mouseEvent->scenePos();
 }
 
 void Canvas::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
 {
     buttonHeld = true;
-    putPixel(mouseEvent->scenePos());
+    if (mouseEvent->buttons() & Qt::LeftButton)
+        putPixel(mouseEvent->scenePos(), Canvas::c1);
+    else if(mouseEvent->buttons() & Qt::RightButton)
+        putPixel(mouseEvent->scenePos(), Canvas::c2);
 }
 
 void Canvas::mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent)
