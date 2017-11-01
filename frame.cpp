166ed479d9f1ecca4cc787455b6729308fd65496
Samuel Teare <samuelteare@samuels-mbp.uconnect.utah.edu>
2017-11-01 11:01:01 -0600

    Added implementation for drawPen, drawDither, drawRectangle, drawEllipse functions in frame class

--- frame.cpp
+++ frame.cpp
@@ -5,7 +5,24 @@ frame::frame(int dimension) {
 }
 
 void frame::drawEllipse(int x1, int y1, int x2, int y2) {
+    int width = abs(x1 - x2);
+    int height = abs(y1 - y2);
 
+    int x, y;
+    if(x1 < x2) {
+        x = x1;
+    }
+    else {
+        x = x2;
+    }
+    if (y1 < y2) {
+        y = y1;
+    }
+    else {
+        y = y2;
+    }
+
+    painter.drawEllipse(x, y, width, height);
 }
 
 void frame::drawMirrorPen(QVector<int> pixels) {
@@ -13,11 +30,30 @@ void frame::drawMirrorPen(QVector<int> pixels) {
 }
 
 void frame::drawPen(QVector<int> pixels) {
-
+    for (int i = 0; i < pixels.size(); i + 2) {
+        painter.drawPoint(pixels[i], pixels[i+1]);
+    }
 }
 
 void frame::drawRectangle(int x1, int y1, int x2, int y2) {
+    int width = abs(x1 - x2);
+    int height = abs(y1 - y2);
 
+    int x, y;
+    if(x1 < x2) {
+        x = x1;
+    }
+    else {
+        x = x2;
+    }
+    if (y1 < y2) {
+        y = y1;
+    }
+    else {
+        y = y2;
+    }
+
+    painter.drawRect(x, y, width, height);
 }
 
 void frame::erase(QVector<int> pixels) {
@@ -29,7 +65,9 @@ void frame::bucketFill(int x, int y) {
 }
 
 void frame::drawDither(QVector<int> pixels) {
-    for (int i = 0; i < pixels.size()/2; i++) {
-        painter.drawPoint(pixels[i], pixels[i+1]);
+    for (int i = 0; i < pixels.size(); i + 2) {
+        if((pixels[i] + pixels[i+1]) % 2 != 0) {
+            painter.drawPoint(pixels[i], pixels[i+1]);
+        }
     }
 }
